8029e37032159e9f072a62076b18f696068f4067
Ryan Paul <ryanbimmer@gmail.com>
2019-05-17 17:34:32 -0400

    Add files via upload

---
+++ RPHackathon2019Bad/RPHackathon2019Bad/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPHackathon2019Bad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test
+        }
+    }
+}
